91f49a649986781e8662e1e2fe52d38bd19be5e0
Lawiet <you@example.com>
2018-08-26 23:00:14 +0800

    router

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,23 @@
 <template>
   <div id="app">
-    <h1>组件A</h1>
-    <div class="btn" @click="onButton">
-      组件A按钮
-    </div>
-    <div class="btn" @click="offButton">清除sessionStorage</div>
-    <children></children>
+    <h1>App</h1>
+    <keep-alive>
+      <router-view/>
+    </keep-alive>
   </div>
 </template>
 
 <script>
-import children from "./children";
 export default {
   data() {
-    return {
-      message: "我组件A内的data传给组件B"
-    };
-  },
-  components: {
-    children
+    return {};
   },
   methods: {
-    onButton() {
-      const DateA = this.message;
-      sessionStorage.setItem("activeName", DateA);
-    },
-    offButton() {
-      // 清除sessionStorage
-      sessionStorage.clear();
-    }
+    onButton() {}
   }
 };
 </script>
 
-<style scoped>
-.btn {
-  min-width: 128px;
-  padding: 0 10px;
-  height: 46px;
-  border-radius: 4px;
-  background-color: #64c255;
-  color: #fff;
-  cursor: pointer;
-  display: inline-flex;
-  justify-content: center;
-  align-items: center;
-}
+<style>
+
 </style>
--- src/children.vue
+++ src/children.vue
@@ -1,24 +1,28 @@
 <template>
   <div>
-    <h2>组件B</h2>
-    <h3>{{msg}}</h3>
+    <h2>children</h2>
+    <div>
+      <span>home组件通过路由过来的ID：</span>
+      <strong>{{id}}</strong>
+    </div>
+    <div>
+      <span>home组件通过路由传过来的msg：</span>
+      <strong>{{msg}}</strong>
+    </div>
   </div>
 </template>
+
 <script>
 export default {
   data() {
     return {
-      msg: ""
-    }
+      msg: this.$route.params.msg,
+      id: this.$route.params.id
+    };
   },
-  created() {
-    console.log(sessionStorage.getItem('activeName'))
-    this.msg = sessionStorage.getItem('activeName')
-  }
-}
-
+  created() {}
+};
 </script>
+
 <style scoped>
-
-
 </style>
